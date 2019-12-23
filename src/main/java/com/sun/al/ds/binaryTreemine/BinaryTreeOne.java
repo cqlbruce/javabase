ca39c1386fe22d4c400ecdd610ae9356a39a5129
陈其亮 <chenqiliang@chenqiliangdeMacBook-Pro.local>
2019-12-23 23:29:39 +0800

    a

---
+++ src/main/java/com/sun/al/ds/binaryTreemine/BinaryTreeOne.java
@@ -0,0 +1,24 @@
+package com.sun.al.ds.binaryTreemine;
+
+public class BinaryTreeOne {
+
+    private Node root ;
+
+    public void insert(){
+
+
+
+    }
+
+    public void delete(){
+
+
+
+    }
+
+    public Node find(){
+
+      return null ;
+    }
+
+}
---
+++ src/main/java/com/sun/al/ds/binaryTreemine/Node.java
@@ -0,0 +1,17 @@
+package com.sun.al.ds.binaryTreemine;
+
+public class Node {
+
+
+    public int key ;
+
+    public Node left ;
+
+    public  Node right;
+
+    public Node(int data){
+        this.key = data ;
+    }
+
+
+}
