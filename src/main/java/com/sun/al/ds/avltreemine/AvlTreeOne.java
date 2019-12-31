22807968bfee9e508ea8384d15d0a67bb2ee4e14
chenqiliang <chenqiliang@newupbank.com>
2019-12-31 17:09:09 +0800

    'a'

--- src/main/java/com/sun/al/ds/avltreemine/AvlTreeOne.java
+++ src/main/java/com/sun/al/ds/avltreemine/AvlTreeOne.java
@@ -1,4 +1,47 @@
 package com.sun.al.ds.avltreemine;
 
-public class AvlTreeOne {
+import com.sun.datastructure.tree.avl.AVLNode;
+
+public class AvlTreeOne<T extends Comparable<T>> {
+
+    Node<T> root ;
+
+    public AvlTreeOne(){
+        root = null ;
+    }
+
+    //获取高度
+    public int height(Node<T> tree){
+        if(tree != null){
+            return tree.height;
+        }
+        return 0 ;
+    }
+
+    public int max(int a , int b){
+        return a>b ? a : b;
+    }
+
+    //查找
+    public Node<T> search(T data ){
+        if(root==null){
+            return null ;
+        }
+        Node<T> current = root ;
+        while (current != null){
+            int a = data.compareTo(current.key);
+            if(a > 0){
+                current = current.right;
+            }else if(a < 0){
+                current = current.left;
+            }else {
+                return current;
+            }
+        }
+        return null ;
+    }
+
+
+
+
 }
