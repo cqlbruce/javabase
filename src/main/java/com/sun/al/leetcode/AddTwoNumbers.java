09d4829c1570b26b4b3c4271dc5514dd7d030f30
chenqiliang <chenqiliang@newupbank.com>
2020-01-11 19:50:41 +0800

    'a'

--- src/main/java/com/sun/al/leetcode/AddTwoNumbers.java
+++ src/main/java/com/sun/al/leetcode/AddTwoNumbers.java
@@ -9,52 +9,110 @@ public class AddTwoNumbers {
     Output: 7 -> 0 -> 8
     Explanation: 342 + 465 = 807
 */
-    class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
-
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(8) ;
+        l1.next = new ListNode(3) ;
+        l1.next.next = new ListNode(6) ;
+        ListNode l2 = new ListNode(6) ;
+        l2.next = new ListNode(2);
+        l2.next.next = new ListNode(9) ;
 
+//        ListNode l1 = new ListNode(9) ;
+//        l1.next = new ListNode(8) ;
+//        ListNode l2 = new ListNode(1) ;
+//        ListNode ll = AddTwoNumbers.addTwoNumOne(l1 , l2);
+        ListNode ll = AddTwoNumbers.addTwoNumTwo(l1 , l2);
 
+        while (true){
+            System.out.print(ll.val);
+            ll = ll.next ;
+            if(ll !=null){
+                System.out.print("->");
+            }else {
+                break;
+            }
+        }
     }
 
-    public ListNode addTwoNumTwo(ListNode l1 , ListNode l2){
+    //s1 时间效率 o(n)
+    public static ListNode addTwoNumOne(ListNode l1 , ListNode l2){
         ListNode l3 = null;
+        ListNode firstNode = null ;
         boolean flag = false;
-        while (l1.next !=null||l2.next != null){
+        while (l1 !=null||l2 != null){
             ListNode l = null  ;
             int a = 0 ;
-            if(null != l1.next){
-                l1 = l1.next ;
+            if(null != l1){
                 a = l1.val ;
+                l1 = l1.next ;
             }
             int b = 0 ;
-            if(null != l2.next){
-                l2 = l2.next ;
+            if(null != l2){
                 b = l2.val;
+                l2 = l2.next ;
             }
             int c = a + b ;
-            if(flag)
+            if(flag){
                 c =  c + 1 ;
-            if(c > 10 ){
+                flag = false ;
+            }
+            if(c >= 10 ){
                 flag = true ;
                 l = new ListNode(c -10);
             }else {
                 l = new ListNode(c);
             }
+            if (l3 == null){
+                firstNode = l ;
+                l3 = l ;
+            }else {
+                l3.next = l ;
+                l3 = l3.next;
+            }
         }
-
-        return  null ;
-
+        if (flag){
+            l3.next = new ListNode(1);
+        }
+        return  firstNode ;
     }
 
 
-
-
-
-
-
+    //s2 时间效率 o(n)
+    //改进空间效率
+    public static ListNode addTwoNumTwo(ListNode l1 , ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result ;
+        boolean flag = false;
+        int sum = 0 ;
+        int a = 0 ;
+        int b = 0 ;
+        while (true) {
+            if(l1 == null && l2 == null && flag==false){
+                break;
+            }
+            a = 0;
+            b = 0;
+            if(null != l1){
+                a = l1.val ;
+                l1 = l1.next ;
+            }
+            if(null != l2){
+                b = l2.val;
+                l2 = l2.next ;
+            }
+            sum = a + b ;
+            if(flag){
+                sum =  sum + 1 ;
+                flag = false ;
+            }
+            if(sum >= 10 ){
+                flag = true ;
+                current = current.next  = new ListNode(sum -10);
+            }else {
+                current = current.next  = new ListNode(sum);
+            }
+        }
+        return  result.next ;
+    }
 
 }
---
+++ src/main/java/com/sun/al/leetcode/ListNode.java
@@ -0,0 +1,10 @@
+package com.sun.al.leetcode;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+
+}
