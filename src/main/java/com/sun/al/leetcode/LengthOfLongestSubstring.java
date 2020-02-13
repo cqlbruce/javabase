0e16995e016ee6dcac541cceebbd81b725b3e65f
陈其亮 <chenqiliang@chenqiliangdeMacBook-Pro.local>
2020-02-13 18:38:37 +0800

    a

---
+++ src/main/java/com/sun/al/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,25 @@
+package com.sun.al.leetcode;
+
+import java.util.Arrays;
+
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    public static int s1(String s1){
+
+        int[] arr = new int[256];
+        Arrays.fill(arr , -1);
+
+
+
+        return 1 ;
+    }
+
+
+}
---
+++ src/main/java/com/sun/al/leetcode/test.java
@@ -0,0 +1,22 @@
+package com.sun.al.leetcode;
+
+public class test {
+
+
+//    给定一个非空字符串 s 和一个包含⾮空单词列列表的字典 wordDict，判定 s 是否可以被空格拆分为⼀个或多个在字典中出现的单词。
+//    说明:
+//            1. 拆分时可以重复使⽤用字典中的单词。 2. 你可以假设字典中没有重复的单词。
+//    示例例 1:
+//    输⼊入: s = "leetcode", wordDict = ["leet", "code"]
+//    输出: true
+//    解释:返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+//    示例例 2:
+//    输⼊入: s = "applepenapple", wordDict = ["apple", "pen"]
+//    输出: true
+//    解释: 返回 true 因为 "applepenapple" 可以被拆分成"apple pen apple"。 注意你可以重复使⽤用字典中的单词。
+//    示例例 3:
+//    输⼊入:s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"] 输出: false
+//
+
+
+}
