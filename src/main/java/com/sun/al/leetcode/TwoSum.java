2723af25ed2624bc4ac3b148cc3b50f0ebc68d20
chenqiliang <chenqiliang@newupbank.com>
2020-03-11 21:46:56 +0800

    'a';

--- src/main/java/com/sun/al/leetcode/TwoSum.java
+++ src/main/java/com/sun/al/leetcode/TwoSum.java
@@ -1,19 +1,22 @@
 package com.sun.al.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TwoSum {
 
 
     public static void main(String[] args) {
-        int[] ints = {9 , 1 , 10 , 3 , 8};
+        int[] ints = {9 , 9 , 1 , 10 , 3 , 3 , 8,2};
         int t = 12 ;
         //int[] indices = twoSumOne(ints , t);
         //int[] indices = twoSum2(ints , t);
-        int[] indices = twoSum3(ints , t);
-        for (int i : indices){
-            System.out.print(i + ",");
+//        int[] indices = twoSum3(ints , t);
+//        for (int i : indices){
+//            System.out.print(i + ",");
+//        }
+        List<List<Integer>> lli = TwoSum.twoSum(ints , t);
+        for (List<Integer> li : lli){
+            System.out.println(li);
         }
     }
 
@@ -59,6 +62,29 @@ public class TwoSum {
             map.put(nums[i], i );
         }
         return null ;
+    }
+
+
+    public static List<List<Integer>> twoSum(int nums[] , int target){
+        List<List<Integer>> lli = new ArrayList<>();
+        Arrays.sort(nums);
+        int low = 0 ;
+        int high = nums.length-1 ;
+        while(low < high){
+            if (nums[low] + nums[high] < target)
+                low ++ ;
+            if (nums[low] + nums[high] > target)
+                high -- ;
+            if (nums[low] + nums[high] == target){
+                List<Integer> li = new ArrayList<Integer>();
+                li.add(nums[low]);
+                li.add(nums[high]);
+                lli.add(li);
+                low++;
+                high--;
+            }
+        }
+        return  lli ;
     }
 
 
