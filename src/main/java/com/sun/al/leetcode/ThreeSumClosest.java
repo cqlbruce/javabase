db99e1c9fb860f758c0f4904839d1b2c7e770931
陈其亮 <chenqiliang@chenqiliangdeMacBook-Pro.local>
2020-03-13 22:04:28 +0800

    a

--- src/main/java/com/sun/al/leetcode/ThreeSumClosest.java
+++ src/main/java/com/sun/al/leetcode/ThreeSumClosest.java
@@ -1,13 +1,25 @@
 package com.sun.al.leetcode;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
 
 
     public static void main(String[] args) {
 
         // Given array nums = [-1, 2, 1, -4], and target = 1.
+        //[-3,-2,-5,3,-4]
+        //-1
+        //[0,2,1,-3]
+        //1
+        //[-1,2,1,-4]
+        //1
+        //[1,2,4,8,16,32,64,128]
+        //82
+        //[-1,2,1,-4]
+        //1
         ThreeSumClosest tsc = new ThreeSumClosest();
-        int[] nums = {-1, 2, 1, -4};
+        int[] nums = {0,2,1,-3};
         System.out.println(tsc.threeSumClosest(nums , 1));
 
     }
@@ -16,20 +28,29 @@ public class ThreeSumClosest {
 
         if (nums==null || nums.length<2)
             return 0 ;
-
-        int closest = 0 ;
-        for (int i=0 ; i<nums.length ; i++){
+        Arrays.sort(nums);
+        int closest = Math.abs(nums[0] + nums[1] + nums[2] - target) ;
+        int res = nums[0] + nums[1] + nums[2] ;
+        for (int i=0 ; i<=nums.length-3 ; i++){
             int low = i + 1 ;
             int high = nums.length - 1 ;
             while (low < high){
                 int sum = nums[i] + nums[low] + nums[high] ;
-                closest = Math.min(closest , Math.abs(sum-target));
-                low++ ;
-                high--;
+                if (Math.abs(sum-target) < closest){
+                    closest = Math.abs(sum-target);
+                    res = sum ;
+                }
+                if (sum > target) {
+                    high--;
+                }else if (sum < target)
+                    low++;
+                else{
+                   return target;
+                }
             }
-        }
-        return closest ;
 
+        }
+        return res ;
     }
 
 }
