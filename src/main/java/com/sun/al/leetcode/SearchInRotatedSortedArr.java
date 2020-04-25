168c2b95c394a334ddb8690229eff6a495558bd0
陈其亮 <chenqiliang@chenqiliangdeMacBook-Pro.local>
2020-04-26 00:03:49 +0800

    a

--- src/main/java/com/sun/al/leetcode/SearchInRotatedSortedArr.java
+++ src/main/java/com/sun/al/leetcode/SearchInRotatedSortedArr.java
@@ -6,32 +6,64 @@ public class SearchInRotatedSortedArr {
 
 
         //[4,5,6,7,0,1,2], target = 0
+        //[1,3,5]
+        //0
+        //[5,1,3]
+        //0
 
         SearchInRotatedSortedArr sis = new SearchInRotatedSortedArr();
-        int[] ss = {4,5,6,7,0,1,2};
-        System.out.println("res========" + sis.search(ss,7));
+        int[] ss = {5,1,3};
+        System.out.println("res========" + sis.search(ss,0));
 
     }
 
     public int search(int[] nums, int target) {
+        if (null==nums||nums.length==0)
+            return -1;
+        if(nums.length==1){
+            if(nums[0]==target)
+                return 0;
+            else
+                return -1 ;
+        }
         int flag = 0 ;
         int start = 0 ;
         int end = nums.length-1 ;
         int index = 0 ;
         while (true){
             index = (start + end)/2;
-            if (nums[index]<=nums[index-1]&&nums[index]<=nums[index+1]){
-                flag=nums[flag];
+            if(index-1<0){
+                flag = 0 ;
                 break;
             }
+
+            if (nums[index]<=nums[index-1]&&nums[index]<=nums[index+1]){
+                flag=index;
+                break;
+            }else if (end == start+1){
+                flag = 0 ;
+                break;
+            }else if (end == start){
+                flag = 0 ;
+                break;
+            }
+
             if (nums[index]<nums[start]){
                 end = index;
             }else if (nums[index]>nums[start]){
                 start=index;
             }
         }
+        if(nums[nums.length-1] == target){
+            return nums.length-1;
+        }
 
-        if (target==nums[0]){
+        if(flag==0){
+            if(nums[0]==target)
+                return 0;
+            start = 1;
+            end = nums.length-1;
+        }else if (target==nums[0]){
             return 0;
         }else if (target>nums[0]){
             start=0;
@@ -43,12 +75,28 @@ public class SearchInRotatedSortedArr {
 
         while (true){
             index = (start + end) / 2 ;
+
             if (nums[index]>target){
                 end = index;
             }else if (nums[index]<target){
                 start = index;
-            }else {
+            }else{
                 return index;
+            }
+            if(start==end){
+                if(nums[start] == target)
+                    return start;
+                else
+                    return -1;
+            }
+            if(end==(start+1)){
+                if (nums[start]==target){
+                    return start;
+                }else if (nums[end] == target){
+                    return end;
+                }else {
+                    return -1;
+                }
             }
         }
     }
