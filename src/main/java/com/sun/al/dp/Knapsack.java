13a13df215ba7e458401feb265c6a8920ec2fda3
陈其亮 <chenqiliang@chenqiliangdeMacBook-Pro.local>
2021-05-19 20:16:00 +0800

    更新

--- src/main/java/com/sun/al/dp/Knapsack.java
+++ src/main/java/com/sun/al/dp/Knapsack.java
@@ -9,15 +9,14 @@ public class Knapsack {
 
 
     public static void main(String[] args) {
-
-
-
+        int[] w = {2 , 3 , 6 , 8 , 9};
+        int[] v = {3 , 6 , 8 , 8 , 3};
+        int capacity = 10 ;
+        System.out.println(maxVaule(w , v , 4 , capacity));
     }
 
 
-
     public static int maxVaule(int[] w , int[] v , int index , int capacity){
-
 
         if (index<0 || capacity<0)
             return 0 ;
@@ -25,10 +24,21 @@ public class Knapsack {
         int res = maxVaule(w , v ,index-1 , capacity);
 
         if(w[index] <= capacity){
-            return Math.max(res , maxVaule(w, v , index-1 , capacity-w[index])) ;
+            return Math.max(res , v[index] + maxVaule(w, v , index-1 , capacity-w[index])) ;
         }
 
         return res ;
+    }
+
+    public static int maxValueDp(int[] w , int[] v , int index , int capacity){
+
+//        for(){
+//
+//
+//        }
+
+        return 0 ;
+
     }
 
 
